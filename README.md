d502949100f0b3c0d3a800372398a627ca0a1a37
Chris LoPresto <chris@chrislopresto.com>
2015-05-28 14:53:19 -0400

    Implement addon
    This is a quick and dirty first pass

--- README.md
+++ README.md
@@ -1,6 +1,51 @@
-# Ember-cli-proxy-live-reload
+# ember-cli-proxy-live-reload
 
-This README outlines the details of collaborating on this Ember addon.
+When using nginx to terminate SSL and reverse proxy appropriate requests to ember-cli, ember-cli's live-reload will not work out of the box.
+
+A configuration that gets live-reload working is implemented by this
+addon plus nginx rules. This addon includes a script tag in index.html
+as defined by the `contentFor` method below. Our standard nginx config
+proxies the script to ember-cli.
+
+When that script is requested, ember-cli will serve up the script in
+the `dynamicScript` method below, which in turn loads the livereload.js
+script.
+
+Some nginx config proxies that to ember-cli's live-reload server running
+in this project on port 37530:
+
+  server {
+    listen 80;
+    listen 443 ssl;
+
+    server_name cloudfront-standin-app.yapp.dev;
+
+    # ...
+
+    location ~ ^/apps/livereload.js {
+      rewrite  ^/apps/(livereload.js)  /$1 break;
+      proxy_pass http://localhost:37530;
+    }
+
+    # ...
+  }
+
+livereload.js is requested with query params that cause it to make a
+secure websocket connection to cloudfront-standin-app.yapp.dev and on
+a port 100 greater than 37530 (37630). nginx config terminates SLL
+and proxies that to ember-cli's live-reload server:
+
+  server {
+    listen 37630 ssl;
+    server_name cloudfront-standin-app.yapp.dev;
+
+    location / {
+      proxy_pass http://localhost:37530;
+      proxy_http_version 1.1;
+      proxy_set_header Upgrade $http_upgrade;
+      proxy_set_header Connection "upgrade";
+    }
+  }
 
 ## Installation
 
--- index.js
+++ index.js
@@ -2,5 +2,66 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-cli-proxy-live-reload'
+  name: 'ember-cli-proxy-live-reload',
+
+  contentFor: function(type) {
+    var liveReloadPort = process.env.EMBER_CLI_INJECT_LIVE_RELOAD_PORT;
+
+    if (liveReloadPort && type === 'head') {
+      var scriptSrc = process.env.EMBER_CLI_PROXY_LIVE_RELOAD_HOST;
+      if (process.env.EMBER_CLI_PROXY_LIVE_RELOAD_BASE_PATH) {
+        scriptSrc += '/' + process.env.EMBER_CLI_PROXY_LIVE_RELOAD_BASE_PATH;
+      }
+      scriptSrc += '/ember-cli-live-reload.js';
+      console.log('$$$$$$ contentFor', scriptSrc);
+      return '<script src="' + scriptSrc + '" type="text/javascript"></script>';
+    }
+  },
+
+  dynamicScript: function(request) {
+    var liveReloadPort = process.env.EMBER_CLI_INJECT_LIVE_RELOAD_PORT;
+    var scriptSrc = process.env.EMBER_CLI_PROXY_LIVE_RELOAD_HOST;
+    if (process.env.EMBER_CLI_PROXY_LIVE_RELOAD_BASE_PATH) {
+      scriptSrc += '/' + process.env.EMBER_CLI_PROXY_LIVE_RELOAD_BASE_PATH;
+    }
+    scriptSrc += '/livereload.js?snipver=1&host=';
+    scriptSrc += process.env.EMBER_CLI_PROXY_LIVE_RELOAD_HOST.replace('https://', '').replace('http://', '');
+    scriptSrc += '&port=' + process.env.EMBER_CLI_PROXY_LIVE_RELOAD_PROXY_PORT;
+    console.log('$$$$$$ dynamicScript', scriptSrc);
+
+    return "(function() {\n " +
+           "var src = '" + scriptSrc + "';\n " +
+           "var script    = document.createElement('script');\n " +
+           "script.type   = 'text/javascript';\n " +
+           "script.src    = src;\n " +
+           "document.getElementsByTagName('head')[0].appendChild(script);\n" +
+           "}());";
+  },
+
+  serverMiddleware: function(config) {
+    var self = this;
+    var app = config.app;
+    var options = config.options;
+    var proxyOptions = options.proxyLiveReload || {};
+    var portOffset = proxyOptions.portOffset || 100;
+    if (!proxyOptions.host) {
+      console.error('EMBER CLI PROXY LIVE RELOAD - Must specify host');
+    }
+    console.log('$$$$$$ proxyOptions', proxyOptions);
+    console.log('$$$$$$ liveReloadPort', options.liveReloadPort);
+
+    if (options.liveReload !== true) { return; }
+
+    process.env.EMBER_CLI_INJECT_LIVE_RELOAD_PORT = options.liveReloadPort;
+    process.env.EMBER_CLI_INJECT_LIVE_RELOAD_BASEURL = options.baseURL; // default is '/'
+    process.env.EMBER_CLI_PROXY_LIVE_RELOAD_PROXY_PORT = parseInt(options.liveReloadPort) + parseInt(portOffset);
+    process.env.EMBER_CLI_PROXY_LIVE_RELOAD_HOST = proxyOptions.host;
+
+    process.env.EMBER_CLI_PROXY_LIVE_RELOAD_BASE_PATH = proxyOptions.basePath || '';
+
+    app.use(options.baseURL + 'ember-cli-live-reload.js', function(request, response, next) {
+      response.contentType('text/javascript');
+      response.send(self.dynamicScript());
+    });
+  }
 };
